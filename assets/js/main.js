b5c1e614b6d3a51f245bcb7744154c0c5109f243
Phu Dinh <--replace-all>
2023-03-23 11:37:33 +0700

    dark

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,13 @@ let btn_dl_pdf = document.getElementById('dl-pdf-btn');
 // get cv-div id="pdf-cv"
 let cv_div = document.getElementById('pdf-cv');
 
-let opt = {
-    margin:         0,
-    image:{type:    "jpeg",quality:1},
-    html2canvas:    {scale:4 },
-    jsPDF:          {format: 'a4', }
-};
-
 function generateRemuse(){
     var opt = {
         margin: 0,
-        filename:       "dinhphuremuse.pdf",
+        filename:       "TruongDinhPhu_Python_0948224950.pdf",
         image: {type: 'jpeg', quality: 1},
-        html2canvas: {scale: 4, dpi: 72, letterRendering: true},
-        jsPDF: {unit: 'in', format: 'A3', orientation: 'portrait'},
+        html2canvas: {scale: 4, dpi: 72},
+        jsPDF: {unit: 'in', format: 'A2'},
         html2canvas: {
             onclone: (element) => {
                 const svgElements = Array.from(element.querySelectorAll('svg'));
@@ -29,7 +22,7 @@ function generateRemuse(){
                     s.setAttribute("height", bBox.height);
                 })
             }
-        }
+        },
     };
     html2pdf().set(opt).from(cv_div).save();
 };
