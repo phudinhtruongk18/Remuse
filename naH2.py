2e95a8bb0801e5ce53f17a2701711d1849863eff
Phu Dinh <--replace-all>
2023-04-02 00:37:32 +0700

    ngu ngon g9

---
+++ naH2.py
@@ -0,0 +1,26 @@
+from typing import List
+import collections
+
+class Solution:
+	def subarraySum(self, nums: List[int], k: int) -> int:
+
+		ans=0
+		prefsum=0
+        # prefix_sums = collections.defaultdict(int)
+		prefix_sums = collections.defaultdict(int)
+
+		for num in nums:
+			prefsum = prefsum + num
+
+			if prefsum-k in prefix_sums:
+				ans = ans + prefix_sums[prefsum-k]
+
+			if prefsum not in prefix_sums:
+				prefix_sums[prefsum] = 1
+			else:
+				prefix_sums[prefsum] = prefix_sums[prefsum]+1
+		print(prefix_sums)
+		return ans
+	
+if __name__ == "__main__":
+	assert Solution().subarraySum([5, 1, 2, 7, 3, 4], 8) == 3
